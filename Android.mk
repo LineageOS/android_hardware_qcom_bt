08c0905bc8d3158827abcc7fa6699990004001f1
Ethan Chen <intervigil@gmail.com>
2014-11-11 23:20:56 -0800

    libbt-vendor: Allow mako to compile libbt-vendor
    Change-Id: I8f841418ff6a196734587d17b8bf8d640cc128c4

--- Android.mk
+++ Android.mk
@@ -1,3 +1,6 @@
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 include $(call all-named-subdir-makefiles,libbt-vendor)
 endif # is-vendor-board-platform
+ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
+include $(call all-named-subdir-makefiles,libbt-vendor)
+endif # for mako
