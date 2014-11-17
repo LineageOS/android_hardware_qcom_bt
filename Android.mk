c5181e7f5a502b9626fd14ff89da0468b40a0509
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-17 00:19:56 +0000

    Remove QCOM platform check from top makefile
    This is redundant, bt-vendor already checks for BOARD_HAVE_BLUETOOTH_QCOM
    
    Change-Id: I8f841418ff6a196734587d17b8bf8d640cc128c4

--- Android.mk
+++ Android.mk
@@ -2,8 +2,6 @@ BT_VENDOR_PATH := $(call my-dir)
 
 ifeq ($(BT_VENDOR_PATH),$(call project-path-for,bt-vendor))
 
-ifeq ($(call is-vendor-board-platform,QCOM),true)
 include $(call all-named-subdir-makefiles,libbt-vendor)
-endif # is-vendor-board-platform
 
 endif
