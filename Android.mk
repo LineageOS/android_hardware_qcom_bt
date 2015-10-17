0ea69fa77f1be8fb85ce976de96be1622fbc6046
Steve Kondik <steve@cyngn.com>
2015-10-16 21:27:31 -0700

    bt: Add guard makefile
    Change-Id: I33d6db9d8368aa8d9458aba0bca06cad08a15eb3

--- Android.mk
+++ Android.mk
@@ -1,4 +1,7 @@
+ifeq ($(call my-dir),$(call project-path-for,bt-vendor))
+
 LOCAL_PATH := $(call my-dir)
+
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
   ifneq ($(filter msm8x27 msm8226,$(TARGET_BOARD_PLATFORM)),)
@@ -8,4 +11,6 @@ ifneq ($(BOARD_IS_AUTOMOTIVE),true)
   else ifneq ($(wildcard $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
   endif
+endif
+
 endif
