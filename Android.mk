a118c8d29e090ebcd406f66e264276bb7e084401
Steve Kondik <steve@cyngn.com>
2015-10-16 21:29:05 -0700

    bt-caf: Add guard makefile
    Change-Id: I89be4bd031c36ce698b7531e9fc66b53c9c13aec

--- Android.mk
+++ Android.mk
@@ -1,3 +1,9 @@
+BT_VENDOR_PATH := $(call my-dir)
+
+ifeq ($(BT_VENDOR_PATH),$(call project-path-for,bt-vendor))
+
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 include $(call all-named-subdir-makefiles,libbt-vendor)
 endif # is-vendor-board-platform
+
+endif
