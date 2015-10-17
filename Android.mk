2a78a7bdc42a8044bd6fe43f313b01a2400bb51f
Steve Kondik <steve@cyngn.com>
2015-10-16 21:27:31 -0700

    bt: Add guard makefile
    Change-Id: I33d6db9d8368aa8d9458aba0bca06cad08a15eb3

--- Android.mk
+++ Android.mk
@@ -1,7 +1,13 @@
+BT_VENDOR_PATH := $(call my-dir)
+
+ifeq ($(BT_VENDOR_PATH),$(call project-path-for,bt-vendor))
+
 ifneq ($(filter msm8960 msm8x27 msm8974 msm8226,$(TARGET_BOARD_PLATFORM)),)
 include $(call all-named-subdir-makefiles,msm8960)
 else
 ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
 include $(call all-named-subdir-makefiles,msm8992)
 endif
+endif
+
 endif
