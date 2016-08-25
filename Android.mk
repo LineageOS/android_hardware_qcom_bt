05c4596c7dbde0bde747cf2819aaf59c1f4fdf61
Chirayu Desai <chirayudesai1@gmail.com>
2016-08-25 15:06:03 +0530

    Revert "msm8974: remove from top level makefile"
    This reverts commit c7dc913784965e4ce705c2045f0a8b43fcd1db1c.
    
    Change-Id: I1162dbf651c02c0ea53a1c1c52412ab810573a21

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
-  ifneq ($(filter msm8x27 msm8226,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter msm8x27 msm8974 msm8226,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,msm8960)
   else ifneq ($(filter msm8994,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,msm8992)
