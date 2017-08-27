d5ac0aa03933076c29af1ae3f1d53a7f39dcd326
TheStrix <parthbhatia@lineageos.org>
2017-08-27 19:32:44 +0530

    bt: Add msm8952 to build guard
     * Use msm8994 stuff for msm8952 compilation
    
    Change-Id: Icfefae14c2a4b8784e47a9ffa00cef010fa7cdb0

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ ifneq ($(BOARD_IS_AUTOMOTIVE),true)
   ifneq ($(filter msm8960 msm8x27 msm8974 msm8226,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,msm8960)
   else
-    ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
+    ifneq ($(filter msm8994 msm8992 msm8952,$(TARGET_BOARD_PLATFORM)),)
       include $(call all-named-subdir-makefiles,msm8992)
     else
       ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
