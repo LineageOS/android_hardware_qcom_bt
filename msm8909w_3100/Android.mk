253a606c588628ed243e339bbbed46e6f9920e35
Michael Bestas <mkbestas@lineageos.org>
2025-01-08 09:10:46 +0200

    bt: Remove unsupported platforms
    Change-Id: I6f1a08da808f17adabbe788e17725d0c67678eef

--- msm8909w_3100/Android.mk
+++
@@ -1,3 +0,0 @@
-ifneq (,$(call is-vendor-board-qcom))
-include $(call all-named-subdir-makefiles,libbt-vendor)
-endif # is-vendor-board-platform
--- msm8992/Android.mk
+++
@@ -1,3 +0,0 @@
-ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
-include $(call all-named-subdir-makefiles,libbt-vendor)
-endif
