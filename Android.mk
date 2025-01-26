1cb9ae4b3e57648d6596e7acca01ffde7b2c13c0
Michael Bestas <mkbestas@lineageos.org>
2025-01-26 12:18:53 +0200

    bt: Remove unused tools
    Change-Id: I0f8a96c17629fceee70855cb7d70b85d538de136

--- Android.mk
+++ Android.mk
@@ -1,15 +1,3 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-bt-vendor))
-
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-TMP_LOCAL_PATH := $(LOCAL_PATH)
-
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-include $(TMP_LOCAL_PATH)/libbt-vendor/Android.mk
-endif # is-vendor-board-platform
-
-include $(TMP_LOCAL_PATH)/tools/Android.mk
-
+include $(call all-named-subdir-makefiles,libbt-vendor)
 endif
--- tools/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
-LOCAL_CFLAGS += -Wno-unused-variable
-LOCAL_CFLAGS += -Wno-unused-parameter
-LOCAL_CFLAGS += -Wno-sometimes-uninitialized
-LOCAL_CFLAGS += -Wno-macro-redefined
-LOCAL_CFLAGS += -Wno-format
