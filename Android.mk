3668362ae71f4e825dca836525c6ea8b2b872981
Michael Bestas <mkbestas@lineageos.org>
2025-09-14 19:22:44 +0300

    bt: Convert libbt-vendor to blueprint
    Change-Id: Ib7f6f8b0cbdc61098df00b06d7936129d785fb4b

--- Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(BOARD_IS_AUTOMOTIVE),true)
-  ifneq ($(wildcard $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
-  endif
-endif
